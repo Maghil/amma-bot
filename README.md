e56811455d3eb25d7cfac335436549b8781887a4
Maghil Vannan M <mmaghil2k@gmail.com>
2024-01-01 22:47:11 +0530

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+reply to nigga
+reply to kys
+reply to kms
+
+
+# Advice
+1. phone use panratha niruthu
+
+
+set up in ec2
---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+import os
+TOKEN = os.getenv('TOKEN')
+intents = discord.Intents(messages=True, message_content=True)
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    print(message)
+    print(message.author)
+
+    #ignore own messages
+    if message.author == client.user:
+        pass
+
+    #if need to converse with dad bot
+    if message.author.id == 503720029456695306:
+        if message.content.startswith("Hi"):
+            await message.channel.send('Hello!')
+
+
+    #advice
+
+    #replies
+    #phone
+
+client.run(TOKEN)
