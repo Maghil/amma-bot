22139f7f344b979db720dc1324597a94853ee8e0
Maghil <mmaghil2k@gmail.com>
2024-03-04 13:02:41 +0530

    added response randomizer

--- main.py
+++ main.py
@@ -2,26 +2,41 @@ import os
 import discord
 from discord import app_commands
 from dotenv import load_dotenv
+import json
+import random
+import re
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-intents = discord.Intents(messages=True, message_content=True)
 
-client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
+GOODBYE_MATCH = "(?:good)? ?bye"
+MY_GUILD = discord.Object(id=843816309791391784)  # replace with your guild id
+
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    # syncing commands
+    # remove guild to sync all
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+
+intents = discord.Intents(messages=True, message_content=True)
+client = MyClient(intents=intents)
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    await tree.sync(guild=discord.Object(id=843816309791391784))
-    print("Ready!")
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
 
 
 @client.event
 async def on_message(message):
-    print(message)
-    print(message.author)
-    print(message.content)
 
     # ignore own messages
     if message.author == client.user:
@@ -30,29 +45,44 @@ async def on_message(message):
     # if need to converse with dad bot
     if message.author.id == 503720029456695306:
         if message.content.startswith("Hi"):
-            await message.channel.send('Hello!')
-
-    # advice
-    if False:
-        await message.channel.send("Stop using phones")
+            with open("data/list.json", "r") as file:
+                json_object = json.load(file)
+            im_response = json_object["im_responese"]
+            await message.channel.send(random.choice(im_response))
 
     # replies
     if message.content.find("dead") > -1 and (message.content.find("server") > -1 or (message.content.find("chat") > -1)):
-        await message.channel.send("put down your phone then")
+        await message.channel.send("poi vellaya paaru")
+
+    for i in ["niga", "nigga", "niggaz", "niggers", "nigger", "snigger", "retard"]:
+        if message.content.find(i) > -1:
+            with open("data/list.json", "r") as file:
+                json_object = json.load(file)
+            slur_response = json_object["slur_response"]
+            await message.channel.send(random.choice(slur_response))
+
+    if message.content.find("boomer") > -1:
+        with open("data/list.json", "r") as file:
+            json_object = json.load(file)
+        boomer_response = json_object["boomer_response"]
+        await message.channel.send(random.choice(boomer_response))
+
+    if re.search(GOODBYE_MATCH,message.content,re.IGNORECASE):
+        with open("data/list.json", "r") as file:
+            json_object = json.load(file)
+        bye_response = json_object["bye_response"]
+        await message.channel.send(random.choice(bye_response))
+
+# Commands
 
 
-    for i in ["niga","nigga","niggaz","niggers","nigger","snigger","retard"]:
-        if message.content.find(i) >-1:
-            await message.channel.send('son lets not use any unecessary slurs, human emotions are complex, if nothing good to say then lets stay quiet')
+@client.tree.command()
+async def advice(interaction: discord.Integration):
+    with open("data/list.json", "r") as file:
+        json_object = json.load(file)
+    advice = json_object["advice"]
+    await interaction.response.send_message(random.choice(advice))
 
-#commands
-@tree.command(
-    name="first_command",
-    description="My first application Command",
-    guild=discord.Object(id=843816309791391784)
-)
-async def first_command(interaction,arg1):
-    await interaction.response.send_message("Hello!")
 
 if __name__ == "__main__":
     client.run(TOKEN)
