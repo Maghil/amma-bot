f5d8a44b308475d5beca6ae4cded7cfcc703aae7
Maghil <mmaghil2k@gmail.com>
2024-10-28 23:22:57 +0530

    improved prompt

--- helper.py
+++ helper.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
 
+
 class AiHelper:
     def __init__(self) -> None:
         self.ai_client = OpenAI(
@@ -12,10 +13,26 @@ class AiHelper:
         )
 
     def getResponse(self, prompt):
+        print(prompt)
         response = self.ai_client.chat.completions.create(
             model="gpt-4o",
             messages=[
                 {"role": "user", "content": prompt}
             ]
         )
-        return response.choices[0].message.content
+        print(response.choices[0].message.content)
+        return "a"
+
+
+if __name__ == "__main__":
+    prompt = "Provide a list of sentences in Tanglish, separated by semi-colons without numbering, as if from a concerned Tamil mom when a child says bye, sorted by clarity and fluency."
+    ai_client = OpenAI(
+            api_key=OPEN_AI_KEY
+        )
+    response=ai_client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    print((response.choices[0].message.content).split(";"))
